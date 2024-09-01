13beff9c248318a9dd272cfbdc2b8ead361ca784
Matthew Cepeda <kharlosj@gmail.com>
2024-09-01 11:41:08 -0500

    customer

---
+++ src/main/java/com/parquesoftti/c26a/service/C26aApplication.java
@@ -0,0 +1,13 @@
+package com.parquesoftti.c26a.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C26aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C26aApplication.class, args);
+	}
+
+}
